1ccd175002f49590714bd1f6f45259b05f2f4972
littled3092 <drevild01801@gmail.com>
2024-01-30 20:00:37 +0800

    一些建議：
    1. `#define` 的變數名稱盡量大寫，並且用底線分隔，例如 `#define MAX_NUM 100`
    2. `class` 的第一個字母大寫，例如 `class Student`
    3. 你的 `Motor` 類別裡面包含三個馬達，你應該要將其分開，例如我使用 `Chassis` 類別包裝左右兩個馬達，並且使用 `Motor` 類別包裝單一馬達
    4. 同理，`Sensor` 類別我改為 `IRSensor` 與 `IRArray` 兩個類別
    5. 你可以注意到我的 `class` 裡面沒有用到任何全域變數，只有用到 member variable，這是一個好習慣，你可以參考看看
    6. 在 `case` 裡面，你用了許多數字來代表各種情況，但是這樣會讓你的程式難以閱讀。我使用了二進制的方式來表示個別感測器，個位數代表右邊，十位數代表中間，百位數代表左邊，例如 `0b101` 代表左右感測器有偵測到，中間沒有偵測到。你可以參考看看
    
    我只是大略寫一下，我沒有很了解你的程式邏輯，所以我沒有寫完整，你可以參考看看。如果有任何問題，歡迎再問我。

--- Final_code_e2/Final_code_e2.ino
+++ Final_code_e2/Final_code_e2.ino
@@ -1,258 +1,222 @@
 // state = 0: auto
 // state = 1: manual
-#include<string.h>
+
+#include <string.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#define Left_motor1 9
-#define Left_motor2 8
-#define Left_pwm 3
-#define Right_motor1 10
-#define Right_motor2 12
-#define Right_pwm 11;
-#define IR_left 5
-#define IR_mid 6
-#define IR_right 7;
+
+#define LEFT_MOTOR1 9
+#define LEFT_MOTOR2 8
+#define LEFT_PWM 3
+#define RIGHT_MOTOR1 10
+#define RIGHT_MOTOR2 12
+#define RIGHT_PWM 11
+#define IR_LEFT 5
+#define IR_MID 6
+#define IR_RIGHT 7
+
+class Motor
+{
+public:
+    Motor(int in1, int in2, int pwm) :
+        _in1(in1), _in2(in2), _pwm(pwm)
+    {
+        pinMode(_in1, OUTPUT);
+        pinMode(_in2, OUTPUT);
+        pinMode(_pwm, OUTPUT);
+    }
+
+    void forward(int speed)
+    {
+        digitalWrite(_in1, HIGH);
+        digitalWrite(_in2, LOW);
+        analogWrite(_pwm, speed);
+    }
+
+    void backward(int speed)
+    {
+        digitalWrite(_in1, LOW);
+        digitalWrite(_in2, HIGH);
+        analogWrite(_pwm, speed);
+    }
+
+private:
+    int _in1;
+    int _in2;
+    int _pwm;
+};
+
+class Chassis
+{
+public:
+    Chassis(Motor& left, Motor& right) :
+        _left(left), _right(right)
+    {}
+
+    void forward(int speed)
+    {
+        _left.forward(speed);
+        _right.forward(speed);
+    }
+
+    void backward(int speed)
+    {
+        _left.backward(speed);
+        _right.backward(speed);
+    }
+
+    void turnLeft(int speed)
+    {
+        _left.backward(speed);
+        _right.forward(speed);
+    }
+
+    void turnRight(int speed)
+    {
+        _left.forward(speed);
+        _right.backward(speed);
+    }
+
+    void stop()
+    {
+        _left.forward(0);
+        _right.forward(0);
+    }
+private:
+    Motor& _left;
+    Motor& _right;
+};
+
+class IRSensor
+{
+public:
+    IRSensor(int pin) : _pin(pin)
+    {
+        pinMode(_pin, INPUT);
+    }
+
+    bool read()
+    {
+        return digitalRead(_pin);
+    }
+
+private:
+    int _pin;
+};
+
+class IRArray
+{
+public:
+    IRArray(IRSensor& left, IRSensor& mid, IRSensor& right) :
+        _left(left), _mid(mid), _right(right)
+    {}
+
+    int read()
+    {
+        bool leftValue = _left.read();
+        bool midValue = _mid.read();
+        bool rightValue = _right.read();
+
+        return (leftValue << 2) | (midValue << 1) | rightValue; 
+    }
+
+private:
+    IRSensor& _left;
+    IRSensor& _mid;
+    IRSensor& _right;
+};
+
 int state = 0;
-int Time1=300,Time2=100;
-char moterstatus,sensorstatus;
-SoftwareSerial BT(4,13);
-class motor
+int time1 = 300;
+int time2 = 100;
+SoftwareSerial BT(4, 13);
+Chassis chassis(
+    Motor(LEFT_MOTOR1, LEFT_MOTOR2, LEFT_PWM),
+    Motor(RIGHT_MOTOR1, RIGHT_MOTOR2, RIGHT_PWM)
+);
+IRArray irArray(
+    IRSensor(IR_LEFT),
+    IRSensor(IR_MID),
+    IRSensor(IR_RIGHT)
+);
+
+void setup()
 {
-  public:
-    motor(int Left_motor1,int Left_motor2,int Left_pwm,int Right_motor1,int Right_motor2,int Right_pwm);
-    void move(int dir);
-  private:
-    int _Left_motor1,_Left_motor2,_Left_pwm,_Right_motor1,_Right_motor2,_Right_pwm;
+    Serial.begin(9600);
+    BT.begin(9600);
 }
-class Sensor
+
+void loop()
 {
-  public:
-    Sensor(int IR_left,int IR_mid,int IR_right);
-    int sensor_value();
-  private:
-    int _IR_left,_IR_mid,_IR_right;
-}
-motor::motor(int Left_motor1,int Left_motor2,int Left_pwm,int Right_motor1,int Right_motor2,int Right_pwm)
-{
-  pinMode(Left_motor1,OUTPUT);
-  pinMode(Left_motor2,OUTPUT);
-  pinMode(Right_motor1,OUTPUT);
-  pinMode(Right_motor2,OUTPUT);
-  pinMode(Left_pwm,OUTPUT);
-  pinMode(Right_pwm,OUTPUT);
-  _Left_motor1=Left_motor1;
-  _Left_motor2=Left_motor2;
-  _Left_pwm=Left_pwm
-  _Right_motor1=Right_motor1
-  _Right_motor2=Right_motor2;
-  _Right_pwm=Right_pwm;
-}
-void motor::move(int dir);
-{
-   if(dir==1)
-  {
-    digitalWrite(Left_motor1,HIGH);
-    digitalWrite(Left_motor2,LOW);
-    digitalWrite(Right_motor1,LOW);
-    digitalWrite(Right_motor2,HIGH);
-    analogWrite(Left_pwm,150);
-    analogWrite(Right_pwm,150);
-    delay(Time2);
-  }
-  else if(dir==0)
-  {
-    digitalWrite(Left_motor1,LOW);
-    digitalWrite(Left_motor2,LOW);
-    digitalWrite(Right_motor1,LOW);
-    digitalWrite(Right_motor2,LOW);
-    analogWrite(Left_pwm,150);
-    analogWrite(Right_pwm,150);
-    delay(Time1);
-  }
-  else if(dir==2)
-  {
-    digitalWrite(Left_motor1,LOW);
-    digitalWrite(Left_motor2,HIGH);
-    digitalWrite(Right_motor1,HIGH);
-    digitalWrite(Right_motor2,LOW);
-    analogWrite(Left_pwm,150);
-    analogWrite(Right_pwm,150);
-    delay(Time2);
-  }
-  else if(dir==3)
-  {
-    digitalWrite(Left_motor1,HIGH);
-    digitalWrite(Left_motor2,LOW);
-    digitalWrite(Right_motor1,HIGH);
-    digitalWrite(Right_motor2,LOW);
-    analogWrite(Left_pwm,0);
-    analogWrite(Right_pwm,200);
-    delay(Time2);
-  }
-  else if(dir==4)
-  {
-    digitalWrite(Left_motor1,LOW);
-    digitalWrite(Left_motor2,HIGH);
-    digitalWrite(Right_motor1,LOW);
-    digitalWrite(Right_motor2,HIGH);
-    analogWrite(Left_pwm,200);
-    analogWrite(Right_pwm,0);
-    delay(Time2);
-  }
-  else if(dir==5)
-  {
-    digitalWrite(Left_motor1,LOW);
-    digitalWrite(Left_motor2,HIGH);
-    digitalWrite(Right_motor1,LOW);
-    digitalWrite(Right_motor2,HIGH);
-    analogWrite(Left_pwm,85);
-    analogWrite(Right_pwm,0);
-    delay(Time2);
-  }
-  else if(dir==6)
-  {
-    digitalWrite(Left_motor1,HIGH);
-    digitalWrite(Left_motor2,LOW);
-    digitalWrite(Right_motor1,HIGH);
-    digitalWrite(Right_motor2,LOW);
-    analogWrite(Left_pwm,0);
-    analogWrite(Right_pwm,85);
-    delay(Time2);
-  }
-}
-Sensor::Sensor(int IR_left,int IR_mid,int IR_right)
-{
-  pinMode(IR_left,INPUT);
-  pinMode(IR_mid,INPUT);
-  pinMode(IR_right,INPUT);
-  _IR_left=IR_left;
-  _IR_mid=IR_mid;
-  _IR_right=IR_right;
-}
-int Sensor::sensor_value()
-{
-  if(digitalRead(IR_mid)==LOW&&digitalRead(IR_right)==HIGH&&digitalRead(IR_left)==LOW)
-  {
-    Serial.println("Forward");
-    return 1;
-  }
-  else if(IR_left_value==HIGH&&IR_right_value==HIGH&&IR_mid_value==HIGH)
-  {
-    Serial.println("TurnLeft_quickly");
-    return 2;
-  }
-  else if(IR_right_value==LOW&&IR_mid_value==HIGH&&IR_left_value==LOW)
-  {
-    Serial.println("TurnRight_quickly");
-    return 3;
-  }
-  else if(IR_mid_value==HIGH&&IR_right_value==HIGH&&IR_left_value==LOW)
-  {
-    Serial.println("Backward");
-    return 4;
-  }
-  else if(IR_right_value==LOW&&IR_mid_value==LOW&&IR_left_value==HIGH)
-  {
-    Serial.println("Stop");
-    return 5;
-  }
-  else if(IR_right_value==LOW&&IR_mid_value==LOW&&IR_left_value==LOW)
-  {
-    Serial.println("TurnRight_slowly");
-    return 6;
-  }
-  else if(IR_left_value==HIGH&&IR_right_value==HIGH&&IR_mid_value==LOW)
-  {
-    Serial.println("TurnLeft_slowly");
-    return 7;
-  }
-  else 
-  {
-    Serial.println("else");
-    return 8;
-  }
-}
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  BT.begin(9600); // 設定和 HC-06 通訊的速度 (預設 9600) 
-}
-void loop() {
-  if(state == 0) // auto
-  {
-    // auto code here
-    sensorstatus=sensor.sensor_value();
-    switch(sensor_value)
+    if(state == 0)
     {
-      case 1:
-        motor.move(2);
-        motor.move(0);
-        break;
-      case 2:
-        motor.move(3);
-        motor.move(0);
-        break;
-      case 3:
-        motor.move(4);
-        motor.move(0);
-        break;
-      case 4:
-        motor.move(6);
-        motor.move(0);
-        motor.move(5);
-        motor.move(0);
-        break;
-      move 5:
-        motor.move(0);
-        state = 1;
-        break;
-      move 6:
-        motor.move(5);
-        motor.move(0);
-        break;
-      move 7:
-        motor.move(6);
-        motor.move(0);
-        break;
-      move 8:
-        motor.move(0);
-        break;
+        // auto code
+        sensorstatus = irArray.read();
+        switch(sensorstatus)
+        {
+        case 0b000:
+            chassis.stop();
+            break;
+
+        case 0b001:
+            chassis.stop();
+            break;
+
+        case 0b010:
+            chassis.stop();
+            break;
+
+        case 0b011:
+            chassis.turnLeft(255);
+            break;
+
+        case 0b100:
+            chassis.stop();
+            break;
+
+        case 0b101:
+            chassis.forward(255);
+            break;
+
+        case 0b110:
+            chassis.turnRight(255);
+            break;
+
+        case 0b111:
+            chassis.backward(255);
+            break;
+        }
     }
-  }
-  else if(state == 1) // manual
-  {
-      // manual code here
-    if (BT.available())
+
+    if (state == 1)
     {
-      moterstatus=BT.read();
-      Serial.println(moterstatus); 
+        // manual code
+        if (BT.available())
+        {
+            char c = BT.read();
+            switch (c)
+            {
+            case 'F':
+                chassis.forward(255);
+                break;
+
+            case 'B':
+                chassis.backward(255);
+                break;
+
+            case 'L':
+                chassis.turnLeft(255);
+                break;
+
+            case 'R':
+                chassis.turnRight(255);
+                break;
+
+            case 'S':
+            case '0':
+                chassis.stop();
+                break;;
+            }
+        }
     }
-    switch (moterstatus)
-    { 
-      case 'F':
-        Serial.println("Forward");
-        motor.move(2);
-        break;
-      case 'B':
-        Serial.println("Backward");
-        motor.move(1);
-        break;
-      case 'L':
-        Serial.println("TurnLeft");
-        motor.move(4);
-        break;
-      case 'R':
-        Serial.println("TurnRight");
-        motor.move(3);
-        break;
-      case 'S':
-        Serial.println("Stop");
-        motor.move(0);
-        break;
-      case '0':
-        Serial.println("Stop");
-        motor.move(0);
-        break;
-    }
-  }
 }
